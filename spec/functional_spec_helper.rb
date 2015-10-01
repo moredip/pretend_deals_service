8d893640a96192f54a1278233bb114914501ffcc
Patrick Turley [pturley] <patrick.turley@gmail.com>
2015-10-01 15:29:05 -0500

    updating the functional test spec helper to handle new rake task interface

--- spec/functional_spec_helper.rb
+++ spec/functional_spec_helper.rb
@@ -4,15 +4,15 @@ require 'net/http'
 
 load File.expand_path("Rakefile")
 
-APP_NAME = ENV["APP_NAME"] || "pretend-deals-service-#{SecureRandom.hex(4)}"
+APP_NAME = ENV["APP_NAME"] || SecureRandom.hex(4)
 MANAGE_DEPLOYED_APP = ENV["BASE_URL"]
-BASE_URL = ENV["BASE_URL"] || "http://#{APP_NAME}.cfapps.io"
+BASE_URL = ENV["BASE_URL"] || "http://#{APP_NAME}_deals.cfapps.io"
 
 raise "You need to have the PRICING_SERVICE_URL or the BASE_URL environment variable set to run these tests" unless ENV["PRICING_SERVICE_URL"] || ENV["BASE_URL"]
 
 RSpec.configure do |rspec|
   rspec.before(:suite) do
-    Rake::Task["app:deploy"].invoke(ENV['RACK_ENV'], APP_NAME, APP_NAME, ENV['PRICING_SERVICE_URL']) unless MANAGE_DEPLOYED_APP
+    Rake::Task["app:deploy"].invoke(ENV['RACK_ENV'], APP_NAME) unless MANAGE_DEPLOYED_APP
   end
 
   rspec.after(:suite) do
