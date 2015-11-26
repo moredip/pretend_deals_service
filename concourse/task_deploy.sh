6e2d10f66b6096f0b2186feb4a872efde804a9e6
Pete Hodgson <git@thepete.net>
2015-11-25 17:59:55 -0800

    deploy task works, sorta?

--- concourse/task_deploy.sh
+++ concourse/task_deploy.sh
@@ -3,8 +3,9 @@ set -e
 
 # TODO: check for required env variables (CF_SPACE)
 
-DEPLOY_GUID=$(ruby -r securerandom -e 'print SecureRandom.hex') # Concourse doesn't give us a build guid :(
-APP_NAME=deals_$DEPLOY_GUID
+DEPLOY_UID=$(ruby -r securerandom -e "print SecureRandom.uuid.split('-').first") # Concourse doesn't give us a build guid :(
+#DEPLOY_UID=12
+APP_NAME=deals$DEPLOY_UID
 CF_ARGS=$CF_SPACE,$APP_NAME
 
 # TODO: don't re-install cf if it's already available in $PATH
@@ -13,4 +14,5 @@ dpkg -i /tmp/cf-cli.deb
 
 bundle install --path=vendor/bundle --without production 
 
-bundle exec rake cf:cups[$CF_ARGS] cf:deploy[$CF_ARGS]
+bundle exec rake cf:cups[$CF_SPACE,$APP_NAME]
+bundle exec rake cf:deploy[$CF_SPACE,$APP_NAME] 
